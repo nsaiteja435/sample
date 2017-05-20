0ecb208209ca3f3cb2100b07f3712a623e00e389
nsait <nsaiteja435@gmail.com>
2017-05-19 20:08:45 -0500

    add helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
-
-	    public static void main(String[] args) {
-		            // Prints "Hello, everyone how r u?" to the terminal window.
-			    //         System.out.println("Hello, World");
-			    //             }
-			    //
-			    //             }
-			    //             
+	   public static void main(String[] args) {
+           System.out.println("Hello, World");
+	 }
+    }

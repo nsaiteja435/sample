8f8e18dae9f2de50fcb3ffb27e3ab89b8995c022
nsait <nsaiteja435@gmail.com>
2017-05-19 22:21:43 -0500

    changes added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class helloworld {
 	   public static void main(String[] args) {
-           System.out.println("Hello, everyone How are you?");
+           System.out.println("Hello, everyone`");
 	 }
     }
